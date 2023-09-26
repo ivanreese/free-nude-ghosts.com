15ef8aaf24719cc93917dca91219e4d9a747a201
Ivan Reese <ivanreese@gmail.com>
2023-09-26 08:40:18 -0600

    ∆

--- netlify/edge-functions/randomize.js
+++ netlify/edge-functions/randomize.js
@@ -2,10 +2,13 @@ export default async (request, context) => {
   const response = await context.next();
   const page = await response.text();
 
-  const n = 5;
-  const r = (Math.random() * n) | 0;
+  const totalImages = 5;
 
-  const updatedPage = page.replace("{{}}", r);
+  const seconds = Date.now() / 1000;
+  const index = seconds / 5; // New image every 5 seconds
+  const image = (index % totalImages) | 0;
+
+  const updatedPage = page.replace("{{}}", image);
 
   return new Response(updatedPage, response);
 };
